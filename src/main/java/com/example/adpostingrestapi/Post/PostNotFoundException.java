0bc7b29bdb43e03afce0f9cb473803a84a48be7e
Ife <sunmolaife@gmail.com>
2022-07-01 23:43:48 -0600

    Fixed typo

--- src/main/java/com/example/adpostingrestapi/Post/PostNotFoundException.java
+++ src/main/java/com/example/adpostingrestapi/Post/PostNotFoundException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class PostNotFoundException extends RuntimeException{
     public PostNotFoundException(long id) {
-        super("Post with id" + id + "was not found");
+        super("Post with id" + id + " was not found");
     }
 }
