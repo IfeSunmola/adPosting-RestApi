30f106d7d97d74549bd2686b691200f82f92dc7d
Ife <sunmolaife@gmail.com>
2022-07-02 01:52:19 -0600

    password is hidden from api calls

--- src/main/java/com/example/adpostingrestapi/User/User.java
+++ src/main/java/com/example/adpostingrestapi/User/User.java
@@ -1,5 +1,6 @@
 package com.example.adpostingrestapi.User;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor // for UserDetails
+@JsonIgnoreProperties("password")
 public class User {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
